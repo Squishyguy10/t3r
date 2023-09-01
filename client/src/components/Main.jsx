3ba73294f13a83477529aa23560f684366e7e506
Squishyguy10 <squishyguy10@Gmail.com>
2023-09-01 16:48:49 -0400

    Moved description below bg img

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -11,7 +11,7 @@ function Main() {
             const windowHeight = window.innerHeight;
             const elementOffset = document.getElementById('fade-in-text').offsetTop;
             
-            scrollTop > elementOffset-windowHeight ? setIsVisible(true) : setIsVisible(false);
+            scrollTop > elementOffset-windowHeight+500 ? setIsVisible(true) : setIsVisible(false);
             console.log(isVisible);
         };
     
@@ -20,31 +20,32 @@ function Main() {
     }, []);
 
     return (
-        <div className='h-screen bg-nature-bg'>
-            <div className='h-screen opacity-90 bg-black/50'>
-                <div className='opacity-100 container px-5 pt-24 pb-10 mx-auto text-center lg:px-40'>
-                    <h1 className='text-7xl pb-10 font-display text-green-400 font-bold'>
-                        THE 3 R'S
-                    </h1>
-
-
-                    {/* What is T3R? */}
-                    <div className='container px-5 pt-40 mx-auto text-center lg:px-40 pb-80'>
-                        <div id="fade-in-text" className={`${isVisible ? 'opacity-100 transition-opacity duration-1000' : 'opacity-0'}`}> 
-                            <div className='flex flex-col w-full mb-40'>
-                                <BookOpenIcon className='mx-auto inline-block w-10 mt-40 text-green-200' />
-                                
-                                <h1 className='sm:text-4xl text-5xl font-medium title-font mb-4 text-green-400 py-3 font-display'>
-                                    What is T3R? 
-                                </h1>
-                                <p className='lg:w-3/4 mx-auto leading-relaxed text-base text-green-200'>
-                                    T3R is an app that aims to make the world a more sustainable place.
-                                    Through the 3Rs principle of sustainability (reducing, reusing, and recycling),
-                                    we implemented multiple innovative solutions to conserve ecological balance, via
-                                    reducing food waste and recycling!
-                                </p>
-                            </div>
-                        </div>
+        <div>
+            <div className='h-screen bg-nature-bg'>
+                <div className='h-screen opacity-90 bg-black/50'>
+                    <div className='opacity-100 container px-5 pt-24 pb-10 mx-auto text-center lg:px-40'>
+                        <h1 className='text-7xl pb-10 font-display text-green-400 font-bold'>
+                            THE 3 R'S
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            
+            {/* What is T3R? */}
+            <div className='container px-5 pt-20 mx-auto text-center lg:px-40 pb-80'>
+                <div id="fade-in-text" className={`transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}> 
+                    <div className='flex flex-col w-full mb-40'>
+                        <BookOpenIcon className='mx-auto inline-block w-10 mt-40 text-green-500' />
+                        
+                        <h1 className='sm:text-4xl text-5xl font-medium title-font mb-4 text-green-600 py-3 font-display'>
+                            What is T3R? 
+                        </h1>
+                        <p className='lg:w-3/4 mx-auto leading-relaxed text-base text-green-500'>
+                            T3R is an app that aims to make the world a more sustainable place.
+                            Through the 3Rs principle of sustainability (reducing, reusing, and recycling),
+                            we implemented multiple innovative solutions to conserve ecological balance, via
+                            reducing food waste and recycling!
+                        </p>
                     </div>
                 </div>
             </div>

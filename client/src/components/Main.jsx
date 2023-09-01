ed40efa126bbf91f909a01e649ad821bc9e20a49
Squishyguy10 <squishyguy10@Gmail.com>
2023-09-01 13:10:51 -0400

    changed bg so no blur but translucent

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -4,8 +4,8 @@ function Main() {
 
     return (
         <div className='h-screen bg-nature-bg'>
-            <div className='h-screen backdrop-blur-[3px] bg-white/30'>
-                <div className='container px-5 pt-20 pb-10 mx-auto text-center lg:px-40'>
+            <div className='h-screen opacity-90 bg-white/30'>
+                <div className='opacity-100 container px-5 pt-20 pb-10 mx-auto text-center lg:px-40'>
 
                     <h1 className='text-7xl pb-10 font-display text-green-400'>
                         The 3 Rs

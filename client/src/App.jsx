f554136bbab0ed29979893c6456922405772811c
Antpoo <1jiangand2@gmail.com>
2023-09-01 12:04:31 -0400

    Fixed navbar, start button
    Making a button component to make life easier. Navbar looks slightly better now.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
+import Button from './components/Button';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
 
           <Route exact path='/'>
             <Main />
+            <Button textcolor='black' color='purple'>
+              Test
+            </Button>
           </Route>
 
         </Switch>

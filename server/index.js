a96d3d0dd314b8cdc9d67ca2ebf507b688cfb465
qualya <102568306+qualya@users.noreply.github.com>
2023-09-01 12:44:50 -0400

    setup mongodb

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const dotenv = require('dotenv');
+dotenv.config();
+
+const pass = process.env.PASSWORD;
+const uri = "mongodb+srv://admin:"+pass+"@t3r.kuzpunr.mongodb.net/?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,7 +15,16 @@ app.get("/api", (req, res) => {
     res.json({ message: 'Hello from server!' });
 });
 
+const start = async() => {
+	try {
+		await mongoose.connect(uri);
+		app.listen(PORT, () => {
+			console.log(`Server listening on ${PORT}`);
+		});
+	}
+	catch (err) {
+		console.log(err.message);
+	}
+}
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+start();

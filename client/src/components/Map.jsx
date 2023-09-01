cec61d9122f6306d58ffac80bd204141bf9561dd
Antpoo <1jiangand2@gmail.com>
2023-09-01 16:01:32 -0400

    Get Map Data
    Successfully get map data from API and display it on the web page.

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
-import GetMap from './GetMap';
+
+const key = 'gaJGz4L6d7qtCcT4E404L0zi7sX9QNWL';
+
 
 class Map extends Component {
 
     constructor(props) {
         super(props);
-        console.log(GetMap());
+        this.state = {
+            mapData: null,
+        };
+    }
+
+    async componentDidMount() {
+        try {
+            const mapData = await this.GetMap();
+            this.setState({ mapData });
+        }
+        catch (error) {
+            console.error('Error fetching map data:', error);
+        }
+    }
+
+    async GetMap() {
+        return await fetch(`https://www.mapquestapi.com/search/v2/radius?key=${key}&maxMatches=4&origin=39.750307,-104.999472`, {
+            method: 'GET',
+        })
+        .then((response) => response.json())
+        .then((response) =>  {
+            console.log(response);
+            return response;
+        });
     }
     
     render() {
+        const { mapData } = this.state;
         return (
             <div>
-
+                {mapData ? (
+                    <div>
+                        {mapData.searchResults.map(result => (
+                            <h1>
+                                {result.name}
+                            </h1>
+                        ))}
+                    </div>
+                ) : (
+                    <p>Farting...</p>
+                )}
             </div>
         );
     }

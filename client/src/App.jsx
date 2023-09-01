cc4d85be775588e4010d734b645506e5f9f7eb44
Squishyguy10 <squishyguy10@Gmail.com>
2023-09-01 12:05:28 -0400

    Added an image for bg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from './components/NavBar';
+import NavBar from './components/Navbar';
 import Main from './components/Main';
 import Button from './components/Button';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';

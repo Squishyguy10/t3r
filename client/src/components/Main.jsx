e01fd78d08be489da3c14242b8eb58478fdc86d9
Squishyguy10 <squishyguy10@Gmail.com>
2023-09-01 13:06:04 -0400

    Added bg (with blur)

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-
 function Main() {
 
     return (
-        <div className="h-screen">
-            
+        <div className='h-screen bg-nature-bg'>
+            <div className='h-screen backdrop-blur-[3px] bg-white/30'>
+                <div className='container px-5 pt-20 pb-10 mx-auto text-center lg:px-40'>
+
+                    <h1 className='text-7xl pb-10 font-display text-green-400'>
+                        The 3 Rs
+                    </h1>
+                </div>
+            </div>
         </div>
     );
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     fontFamily: {
       'display': ['Source Sans 3', 'Arial']
     },
+    backgroundImage: {
+      'nature-bg': "url('../public/naturebg.jpg')",
+    },
     extend: {},
   },
   plugins: [],

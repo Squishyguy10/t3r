95144e5b7355a888d177ea106d641007e6cebfdb
qualya <102568306+qualya@users.noreply.github.com>
2023-09-02 02:39:14 -0400

    add supermarket items formatting to client-test.js

--- server/client-test.js
+++ server/client-test.js
@@ -1,7 +1,25 @@
 fetch('http://localhost:3001/supermarkets')
 	.then((response) => response.json())
 	.then((supermarkets) => {
-		console.log('Supermarkets Data:', supermarkets);
+		const storeNames = [];
+		const itemDetails = [];
+
+		supermarkets.forEach((supermarket) => {
+			const storeName = supermarket.name;
+			storeNames.push(storeName);
+
+			supermarket.inventory.forEach((item) => {
+				itemDetails.push({
+					store: storeName,
+					name: item.name,
+					price: item.price,
+					expiry: item.expiry,
+					quantity: item.quantity,
+				});
+			});
+		});
+		console.log('Store Names:', storeNames);
+		console.log('Item Details:', itemDetails);
 	})
 	.catch((error) => {
 		console.error('Error retrieving supermarkets:', error);

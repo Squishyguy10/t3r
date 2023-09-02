40ce0bfaadb7815e415ea8690c5999b3266ab3a4
Squishyguy10 <squishyguy10@Gmail.com>
2023-09-01 22:27:35 -0400

    added more descriptions to main
    Added supermarket description. Recycling will be done soon

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,21 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { BookOpenIcon, ChevronDoubleDownIcon } from '@heroicons/react/24/outline';
+import React from 'react';
+import { BookOpenIcon, ChevronDoubleDownIcon, ShoppingCartIcon, WrenchScrewdriverIcon } from '@heroicons/react/24/outline';
 import { HashLink } from 'react-router-hash-link';
+import FadeInText from './FadeInText';
 
 function Main() {
-    // fade in text
-    const [isVisible, setIsVisible] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            const textOffset = document.getElementById('fade-in-text').offsetTop;
-            window.scrollY > textOffset-window.innerHeight+500 ? setIsVisible(true) : setIsVisible(false);
-        };
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    });
-
-
     return (
         <div>
             <div className='h-screen bg-nature-bg'>
@@ -28,7 +16,7 @@ function Main() {
                             MAKING THE PLANET BETT3R
                         </h1>
                         <div className="mt-60">
-                            <HashLink smooth to='/#about'>
+                            <HashLink smooth to='/#about-t3r'>
                                 <ChevronDoubleDownIcon className='inline-block w-10 text-green-100 animate-bobbing' />
                             </HashLink>
                         </div>
@@ -38,11 +26,10 @@ function Main() {
 
             {/* What is T3R? */}
             <div className="bg-vine-bg">
-                <div id="about" className='container px-5 pt-16 mx-auto text-center lg:px-40 pb-80'>
-                    <div id="fade-in-text" className={`transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}> 
+                <div id="about-t3r" className='container px-5 pt-16 mx-auto text-center lg:px-40 pb-80'>
+                    <FadeInText>
                         <div className='flex flex-col w-full mb-40'>
                             <BookOpenIcon className='mx-auto inline-block w-10 mt-40 text-green-700' />
-                            
                             <h1 className='sm:text-4xl font-medium title-font mb-4 text-green-800 py-3 font-display'>
                                 What is T3R? 
                             </h1>
@@ -53,8 +40,51 @@ function Main() {
                                 reducing food waste and recycling!
                             </p>
                         </div>
+                    </FadeInText>
+                    <div className="mt-20">
+                        <HashLink smooth to='/#our-supermarket-program'>
+                            <ChevronDoubleDownIcon className='inline-block w-10 animate-bobbing' />
+                        </HashLink>
                     </div>
                 </div>
+            </div>
+
+            <div id="our-supermarket-program" className='container px-5 pt-16 mx-auto text-center lg:px-40 pb-80'>
+                <FadeInText>
+                    <div className='flex flex-col w-full mb-40'>
+                        <ShoppingCartIcon className='mx-auto inline-block w-10 mt-40 text-green-700' />
+                        <h1 className='sm:text-4xl font-medium title-font mb-4 text-green-800 py-3 font-display'>
+                            Our Supermarket Program
+                        </h1>
+                        <p className='lg:w-3/5 sm:text-xl mx-auto leading-relaxed text-base text-green-700'>
+                            Over one third of all food products produced yearly end up wasted,
+                            a lot of which come from supermarkets. With over 3 billion people
+                            not being to afford a healthy diet, we wanted someway to prevent such food waste
+                            and make the world more sustainable. Thus, we decided to create a program
+                            where companies can list food for lower prices (that are nearing the expiration date)
+                            and individuals can view the listed items and store locations nearby for pickup. 
+                        </p>
+                    </div>
+                </FadeInText>
+                <div className="mt-20">
+                    <HashLink smooth to='/#our-recycling-program'>
+                        <ChevronDoubleDownIcon className='inline-block w-10 animate-bobbing' />
+                    </HashLink>
+                </div>
+            </div>
+
+            <div id="our-recycling-program" className='container px-5 pt-16 mx-auto text-center lg:px-40 pb-80'>
+                <FadeInText>
+                    <div className='flex flex-col w-full mb-40'>
+                        <WrenchScrewdriverIcon className='mx-auto inline-block w-10 mt-40 text-green-700' />
+                        <h1 className='sm:text-4xl font-medium title-font mb-4 text-green-800 py-3 font-display'>
+                            Our Recycling Program
+                        </h1>
+                        <p className='lg:w-3/5 sm:text-xl mx-auto leading-relaxed text-base text-green-700'>
+                            Fortnite ella babagee
+                        </p>
+                    </div>
+                </FadeInText>
             </div>
         </div>
     );

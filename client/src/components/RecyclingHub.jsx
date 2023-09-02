1c560cfe11b1416ddec7687dd54bd0278f8bd8d5
Squishyguy10 <squishyguy10@Gmail.com>
2023-09-02 16:46:56 -0400

    changed button for recycling hub

--- client/src/components/RecyclingHub.jsx
+++ client/src/components/RecyclingHub.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function RecyclingHub() {
     return (
-        <div className='container px-5 mx-auto text-center lg:px-40 h-screen'>
+        <div className='container px-5 mx-auto text-center lg:px-40'>
             <div className='flex flex-col w-full mb-10'>
                 <ArrowPathIcon className='mx-auto inline-block w-10 py-10' />
                 <h1 className='sm:text-4xl text-3xl font-medium title-font font-display'>
@@ -12,17 +12,21 @@ function RecyclingHub() {
                 </h1>
             </div>
 
-            <h1 className='text-2xl font-medium mb-10'>Complete the survey to get recommendations:</h1>
-
-            {/*Buttons*/}
-            <div className='pb-5'>
-                <Link to='/survey'>
-                    <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded'>
-                        Begin Questionnaire
-                    </button>
-                </Link>
+            {/* <h1 className='text-2xl font-medium mb-10'></h1> */}
+            <div className='p-[30%] pt-4'>
+                <div className='shadow-md shadow-gray-600 rounded-md bg-[url("../public/question.jpg")] bg-none bg-center'>
+                    <h1 className='text-2xl pt-[6%] font-bold pb-[70%]'>
+                        Complete the survey to get recommendations:
+                    </h1>
+                    <div className='pb-10'>
+                        <Link to='/survey'>
+                            <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded'>
+                                Get Started <span className='ml-1'> ➤ </span>
+                            </button>
+                        </Link>
+                    </div>
+                </div>
             </div>
-
         </div>
             
     );

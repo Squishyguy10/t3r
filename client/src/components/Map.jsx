ff405a214e01fd8722ec38d62a9222ae6909e82f
Antpoo <1jiangand2@gmail.com>
2023-09-02 23:26:32 -0400

    Map pins and stuff
    MAP FINALLY WORKS!

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,70 +1,100 @@
-import React, { useState } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { Component } from 'react';
 import Key from './google-maps-api-key';
 
-const containerStyle = {
-    width: '400px',
-    height: '400px',
-};
+class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            map: null,
+            markers: [],
+            infoWindows: [],
+            searchQuery: '',
+            lat: props.lat,
+            lng: props.lng,
+        };
+    }
 
-const center = {
-    lat: 43.3187363,
-    lng: -79.8416009,
-};
+    componentDidMount() {
 
-const GoogleMaps = () => {
-    const [map, setMap] = useState(null);
-    const [searchQuery, setSearchQuery] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
+        if (typeof window.google === 'undefined' || typeof window.google.maps === 'undefined') {
+            this.loadGoogleMapsAPI();
+        } else {
+            this.searchRecyclingLocations();
+        }
+    }
 
-    const handleSearch = () => {
-        if (!map) return;
+    loadGoogleMapsAPI = () => {
+        const script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${Key()}&libraries=places&callback=initMap`;
+        script.async = true;
+        document.head.appendChild(script);
+        script.onload = this.searchRecyclingLocations;
+    };
+
+    searchRecyclingLocations = () => {
+        const center = { lat: this.state.lat, lng: this.state.lng }; 
+
+        const map = new window.google.maps.Map(document.getElementById('map'), {
+            center: center,
+            zoom: 12,
+        });
 
         const service = new window.google.maps.places.PlacesService(map);
         const request = {
-            query: searchQuery,
             location: center,
-            radius: 1000, // Adjust the radius as needed
+            radius: 10000, 
+            keyword: 'Recycling', 
         };
 
-        service.textSearch(request, (results, status) => {
+        service.nearbySearch(request, (results, status) => {
             if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-                setSearchResults(results);
+
+                const newMarkers = results.map((result) => {
+                    const marker = new window.google.maps.Marker({
+                        position: {
+                            lat: result.geometry.location.lat(),
+                            lng: result.geometry.location.lng(),
+                        },
+                        title: result.name,
+                        map: map,
+                    });
+
+
+                    const infoWindow = new window.google.maps.InfoWindow({
+                        content: `<div><strong>${result.name}</strong><br>${result.vicinity}</div>`,
+                    });
+
+                    marker.addListener('click', () => {
+                        this.closeAllInfoWindows();
+                        infoWindow.open(map, marker);
+                    });
+
+                    return marker;
+                });
+
+                this.setState({ markers: newMarkers });
+            } else {
+                console.error('Nearby search request failed with status:', status);
             }
         });
+
+        this.setState({ map });
     };
 
-    return (
-        <div>
-            <div>
-                <input
-                    type="text"
-                    placeholder="Search for buildings..."
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                />
-                <button onClick={handleSearch}>Search</button>
-            </div>
-            <LoadScript googleMapsApiKey={Key()}>
-                <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={14}
-                    onLoad={(map) => setMap(map)}
-                >
-                    {searchResults.map((result) => (
-                        <Marker
-                            key={result.place_id}
-                            position={{
-                                lat: result.geometry.location.lat(),
-                                lng: result.geometry.location.lng(),
-                            }}
-                        />
-                    ))}
-                </GoogleMap>
-            </LoadScript>
-        </div>
-    );
-};
+    closeAllInfoWindows = () => {
+        this.state.infoWindows.forEach((infoWindow) => {
+            infoWindow.close();
+        });
+    };
+    
 
-export default GoogleMaps;
+    render() {
+        return (
+
+            <div id="map" style={{ width: '100%', height: '400px' }} />
+
+        );
+    }
+}
+
+export default Map;
--- client/src/components/Recommendations.jsx
+++ client/src/components/Recommendations.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import { loadGoogleMapsAPI } from './google-maps-api';
 import { ListBulletIcon, MapPinIcon } from '@heroicons/react/24/outline';
+import Key from './google-maps-api-key';
+import Map from './Map';
 
 class Recommendations extends Component {
 
@@ -7,8 +11,11 @@ class Recommendations extends Component {
 		super(props);
 		this.state = {
 			gptMsg: 'Loading...',
+			googleMaps: null,
+			location: null,
+			place: '',
 		};
-		
+
 		if (!Recommendations.executeSecond) {
 			Recommendations.executeSecond = true;
 		}
@@ -19,16 +26,16 @@ class Recommendations extends Component {
 			fetch('http://localhost:3001/get_survey_results', {
 				method: 'POST',
 				headers: {
-				'Content-Type': 'application/json',
+					'Content-Type': 'application/json',
 				},
-				body: JSON.stringify({uuid: user_id}),
+				body: JSON.stringify({ uuid: user_id }),
 			})
 				.then((response) => response.json())
 				.then((data) => {
 					console.log(data);
 					if (data.result) {
 						console.log(data.result);
-						this.setState({gptMsg: data.result});
+						this.setState({ gptMsg: data.result });
 					}
 					else {
 						console.error('Survey response not found.');
@@ -40,8 +47,32 @@ class Recommendations extends Component {
 		}
 	}
 
+	componentDidMount() {
+
+		loadGoogleMapsAPI(Key())
+			.then((maps) => {
+				this.setState({ googleMaps: maps });
+			})
+			.catch((error) => {
+				console.error('Error loading Google Maps API:', error);
+			});
+	}
+
+	handleSelect = async (selectedAddress) => {
+		try {
+			const results = await geocodeByAddress(selectedAddress);
+			const latLng = await getLatLng(results[0]);
+			this.setState({ location: latLng });
+			this.setState({ place: selectedAddress });
+			console.log('Selected location:', latLng);
+		}
+		catch (error) {
+			console.error('Error selecting location:', error);
+		}
+	};
 
 	render() {
+		const { googleMaps } = this.state;
 		return (
 			<div className='container px-5 mx-auto text-center lg:px-40'>
 				<div className='flex flex-col w-full mb-10'>
@@ -50,13 +81,55 @@ class Recommendations extends Component {
 						Recommendations
 					</h1>
 
-					<p className='text-lg pb-52'>{this.state.gptMsg}</p>
+					<p className='text-lg pb-40'>{this.state.gptMsg}</p>
 				</div>
 				<div className='flex flex-col w-full mb-10'>
 					<MapPinIcon className='mx-auto inline-block w-10 py-10' />
-					<h1 className='sm:text-4xl text-3xl font-medium title-font font-display pb-10'>
+					<h1 className='sm:text-4xl text-3xl font-medium title-font font-display pb-5'>
 						Nearby Facilities
 					</h1>
+					<div>
+						{googleMaps &&
+							<div>
+								<PlacesAutocomplete
+									value={this.state.place}
+									onChange={(newPlace) => { this.setState({ place: newPlace }) }}
+									onSelect={this.handleSelect}
+								>
+									{({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+										<div className='flex flex-col py-2 pb-48'>
+											<input
+												{...getInputProps({
+													type: 'search',
+													placeholder: 'Location',
+													className: 'border p-2',
+													size: '40',
+												})}
+											/>
+											<div>
+												{loading ? <div>Loading...</div> : null}
+												{suggestions.map((suggestion) => {
+													const className = suggestion.active
+														? 'suggestion-item--active'
+														: 'suggestion-item';
+													return (
+														<div
+															{...getSuggestionItemProps(suggestion, {
+																className,
+															})}
+														>
+															{suggestion.description}
+														</div>
+													);
+												})}
+											</div>
+										</div>
+									)}
+								</PlacesAutocomplete>
+							</div>
+						}
+						{this.state.location != null ? <Map lat={this.state.location.lat} lng={this.state.location.lng} /> : <></>}
+					</div>
 				</div>
 			</div>
 		);
